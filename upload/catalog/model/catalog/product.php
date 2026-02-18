f828c1434ce32deb251427e63537b8d545285301
tomashina <tomislav@agmedia.hr>
2026-02-18 15:16:19 +0100

    fix category sort by picpath

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -233,15 +233,14 @@ class ModelCatalogProduct extends Model {
             }
         } else {
 
-            // DEFAULT sort: UPC path (folder + numeric filename)
-            $folder_sql = "LEFT(p.upc, LENGTH(p.upc) - LENGTH(SUBSTRING_INDEX(p.upc, '/', -1)))";
+            // DEFAULT sort: numeric UPC filename only
             $base_sql   = "SUBSTRING_INDEX(SUBSTRING_INDEX(p.upc, '/', -1), '.', 1)";
             $num_sql    = "CASE
                       WHEN $base_sql REGEXP '^[0-9]+$' THEN CAST($base_sql AS UNSIGNED)
                       ELSE 999999999999999999
                    END";
 
-            $sql .= " ORDER BY " . $folder_sql . ", " . $num_sql;
+            $sql .= " ORDER BY " . $num_sql;
         }
 
         if (isset($data['order']) && ($data['order'] == 'DESC')) {

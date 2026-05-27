dee3dd87e3bb592fedaa090dae638a45aff59ad2
tomashina <tomislav@agmedia.hr>
2026-05-27 14:24:18 +0200

    price front

--- upload/catalog/language/hr-hr/checkout/success.php
+++ upload/catalog/language/hr-hr/checkout/success.php
@@ -7,11 +7,11 @@ $_['heading_title']        = 'Hvala vam na povjerenju!';
 $_['text_basket']          = 'Košarica';
 $_['text_checkout']        = 'Naplata';
 $_['text_success']         = 'Uspješno';
-$_['text_customer']        = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p><p>Ponudu te podatke za uplatu šaljemo u najkraćem mogućem roku.</a>.</p><p>Zahvaljujemo se na vašoj narudžbi!</p>';
-$_['text_guest']           = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p><p>Ponudu te podatke za uplatu šaljemo u najkraćem mogućem roku.</a>.</p><p>Zahvaljujemo se na vašoj narudžbi!</p>';
+$_['text_customer']        = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p><p>Zahvaljujemo se na vašoj narudžbi!</p>';
+$_['text_guest']           = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p></p><p>Zahvaljujemo se na vašoj narudžbi!</p>';
 
 $_['text_tnx']           ='Hvala vam na povjerenju!';
-$_['text_pouzece']           = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p><p>Ponudu te podatke za uplatu šaljemo u najkraćem mogućem roku.</a>.</p><p>Zahvaljujemo se na vašoj narudžbi!</p>';
+$_['text_pouzece']           = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p></p><p>Zahvaljujemo se na vašoj narudžbi!</p>';
 
 $_['text_bank']           = '<p>Uredno smo zaprimili Vašu narudžbu broj %s i zahvaljujemo Vam.</p>';
 
